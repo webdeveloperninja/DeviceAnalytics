29ea974f123ded010de7cfa5823b2505d035d943
Your Name <rsmith5901@gmail.com>
2019-12-01 19:56:14 -0500

    sdasdf

--- Components/src/app/components/events-list/events-list.component.ts
+++ Components/src/app/components/events-list/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -16,7 +17,7 @@ export class EventsListComponent implements OnInit {
 
   onGetEventsClick() {
     this.http
-      .get('http://localhost:7071/api/get-device-events', {
+      .get(environment.getDeviceEventsApiUrl, {
         params: { deviceId: '20001f001247343438323536', date: '12/1/2019' }
       })
       .subscribe(d => {
--- Components/src/environments/environment.prod.ts
+++ Components/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  getDeviceEventsApiUrl:
+    'https://deviceanalyticsprocessor.azurewebsites.net/api/get-device-events?code=4DMWgMV/B1ghkcJrXb6sE/7KxZ1HZViCbcNoIIaOvnG17wS0sM78jg=='
 };
