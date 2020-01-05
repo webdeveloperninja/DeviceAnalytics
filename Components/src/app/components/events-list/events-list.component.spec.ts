e60683224ad59e3ae6748ee422db17bf7ccd9a6c
Your Name <rsmith5901@gmail.com>
2020-01-05 18:57:59 -0500

    asfdfds

--- Components/src/app/components/events-list/events-list.component.spec.ts
+++ Components/src/app/components/events-list/events-list.component.spec.ts
@@ -4,14 +4,19 @@ import { EventsListComponent } from './events-list.component';
 import { ThemeModule } from 'src/app/theme/theme.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxAdalService } from 'ngx-adal-8';
 
 describe('EventsListComponent', () => {
   let component: EventsListComponent;
   let fixture: ComponentFixture<EventsListComponent>;
+  let adalAuthServiceSpy: jasmine.SpyObj<NgxAdalService>;
 
   beforeEach(async(() => {
+    adalAuthServiceSpy = jasmine.createSpyObj('NgxAdalService', ['login']);
+
     TestBed.configureTestingModule({
       imports: [ThemeModule, NoopAnimationsModule, HttpClientTestingModule],
+      providers: [{ provide: NgxAdalService, useValue: adalAuthServiceSpy }],
       declarations: [EventsListComponent]
     }).compileComponents();
   }));
